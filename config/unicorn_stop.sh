9f8a952431b38edfc72bc1a4c8918480977be78c
mydearxym <mydearxym@qq.com>
2014-06-26 08:52:27 +0800

    cap testing impove unicorn_stop.sh

--- config/unicorn_stop.sh
+++ config/unicorn_stop.sh
@@ -1,14 +1,16 @@
 #! /bin/sh
 
+
 echo $1
 pid_file=$1
 echo "get unicorn pid: # " $pid_file
 if [ -f $pid_file ] ;then
-  echo "found unicorn.pid"
-  u_pid=`ls $pid_file`
+  echo "found $pid_file"
+  u_pid=`cat $pid_file`
+  echo "u_pid: $u_pid"
   {
     echo "kill unicorn"
-    kill -9 `cat $u_pid`
+    kill -9 $u_pid
   } || {
     echo "cat't kill unicorn anyway"
   }
